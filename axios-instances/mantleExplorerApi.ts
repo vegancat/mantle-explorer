aac4106351b61623fa831ce3c803198ecf26574d
Vegan Cat <mmostafavi247@gmail.com>
2023-02-21 13:05:45 +0330

    Add axios instance to mantel explorer api

---
+++ axios-instances/mantleExplorerApi.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const mantleExplorerApiInstance = () =>
+  axios.create({
+    baseURL: "https://explorer.testnet.mantle.xyz/api",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+export default mantleExplorerApiInstance;
