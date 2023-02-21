71b7da1dc983b578ff4443543299080fd8d87d84
Vegan Cat <mmostafavi247@gmail.com>
2023-02-21 22:36:35 +0330

    Add fetching tokens data

--- axios-instances/mantleExplorerApi.ts
+++ axios-instances/mantleExplorerApi.ts
@@ -1,11 +1,10 @@
 import axios from "axios";
 
-const mantleExplorerApiInstance = () =>
-  axios.create({
-    baseURL: "https://explorer.testnet.mantle.xyz/api",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+const mantleExplorerApiInstance = axios.create({
+  baseURL: "https://explorer.testnet.mantle.xyz/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
 export default mantleExplorerApiInstance;
--- pages/address/[address]/index.tsx
+++ pages/address/[address]/index.tsx
@@ -1,20 +1,102 @@
 import { Box, Button } from "@mui/material";
-import React, { useState, useMemo, useCallback } from "react";
+import React, { useState, useMemo, useCallback, useEffect } from "react";
 import { useRouter } from "next/router";
 import Head from "next/head";
 import Modal from "../../../components/Modal";
 import { APIUrl, PageUrl } from "../../../constants";
 import QrCode from "../../../components/QrCode";
+import { useEnsName, useEnsAvatar } from "wagmi";
+import Image from "next/image";
+import _ from "lodash";
+import { isAddress } from "@ethersproject/address";
+import mantleExplorerApiInstance from "../../../axios-instances/mantleExplorerApi";
 
 type Props = {};
 
+type FetchedToken = {
+  balance: string;
+  contractAddress: string;
+  decimals: string;
+  name: string;
+  symbol: string;
+  type: string;
+};
+
 const AddressDetails = (props: Props) => {
   const [openModal, setOpenModal] = useState(false);
+  const [bitBalance, setBitBalance] = useState(0);
+  const [listOfTokens, setListOfTokens] = useState<FetchedToken[]>([]);
+  const [isAddressValid, setIsAddressValid] = useState(true);
 
-  const {
+  let {
     asPath,
     query: { address },
   } = useRouter();
+
+  useEffect(() => {
+    setIsAddressValid(isAddress(address as string));
+  }, [address]);
+
+  const {
+    data: ensName,
+    isError: ensNameError,
+    isLoading: ensNameLoading,
+  } = useEnsName({
+    // @ts-ignore
+    address,
+  });
+
+  const {
+    data: ensAvatar,
+    isError: ensAvatarError,
+    isLoading: ensAvatarLoading,
+  } = useEnsAvatar({
+    // @ts-ignore
+    address,
+  });
+
+  useEffect(() => {
+    const fetchBitBalance = async () => {
+      const { data } = await mantleExplorerApiInstance.post("", null, {
+        params: {
+          module: "account",
+          action: "eth_get_balance",
+          address: address,
+        },
+      });
+
+      setBitBalance(+(parseInt(data.result, 16) / 10 ** 18).toPrecision(2));
+    };
+
+    const fetchListOfTokens = async () => {
+      const { data } = await mantleExplorerApiInstance.post("", null, {
+        params: {
+          module: "account",
+          action: "tokenlist",
+          address: address,
+        },
+      });
+      console.log("🚀 ~ file: index.tsx:63 ~ fetchListOfTokens ~ data", data);
+
+      setListOfTokens(data.result);
+    };
+
+    const fetchTotalTransactions = async () => {
+      const { data } = await mantleExplorerApiInstance.post("", null, {
+        params: {
+          module: "account",
+          action: "tokenlist",
+          address: address,
+        },
+      });
+      console.log("🚀 ~ file: index.tsx:63 ~ fetchListOfTokens ~ data", data);
+
+      setListOfTokens(data.result);
+    };
+
+    fetchBitBalance();
+    fetchListOfTokens();
+  }, [address]);
 
   const handleOpen = useCallback(() => {
     setOpenModal(true);
@@ -33,8 +115,7 @@ const AddressDetails = (props: Props) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* <Box>Address Details: address: {`${address}`}</Box> */}
-      {/* <Modal open={openModal} setOpen={setOpenModal} /> */}
+
       <QrCode open={openModal} setOpen={setOpenModal} url={qrCodeUrl} />
 
       <Box
@@ -49,10 +130,25 @@ const AddressDetails = (props: Props) => {
           <Box>
             <Button onClick={handleOpen}>QR code</Button>
           </Box>
-          <Box>Address in hex format</Box>
-          <Box>Address image icon | possible Ens name or avatar</Box>
-          <Box>Bit balance</Box>
+          <Box>Address in hex format: {address}</Box>
+          <Box>
+            {/* TODO: Render ens part conditionally  **/}
+            {!_.isNil(ensName) && <Box>{ensName}</Box>}
+            {!_.isNil(ensAvatar) && (
+              <Box>
+                <Image src={ensAvatar!} alt="ENS avatar" />
+              </Box>
+            )}
+          </Box>
+          <Box>Bit balance: {bitBalance}</Box>
           <Box>Other Token Holdings</Box>
+          <Box>
+            {listOfTokens.map((token) => (
+              <Box key={token.contractAddress}>
+                {token.symbol}: {token.balance}
+              </Box>
+            ))}
+          </Box>
           <Box>Total Transactions</Box>
           <Box>Total Gas spent</Box>
           <Box>Average Gas per Tx</Box>
